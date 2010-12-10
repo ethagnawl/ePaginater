41c3b6e87045b0e4cff1decfc513fb644546f770
pete <pete@pete-laptop.(none)>
2010-12-10 08:00:01 -0500

    breaking styles out of js

--- jquery.ePaginater.js
+++ jquery.ePaginater.js
@@ -1,10 +1,10 @@
 /*!
 *   ePaginater jQuery Plugin
-*   V0.9
+*   V0.9.9
 *   ethagnawl@gmail.com
 *   http://ethagnawl.com/ePaginater
 *   Copyright 2010, Pete Doherty
-*   Date: 09/04/2010 21:19:34 (EST)
+*   Date: 12/10/2010 21:19:34 (EST)
 *
 *   ePaginater is distributed under the terms of the GNU General Public License.
 *
@@ -23,29 +23,21 @@
 
 ( function ($) {
     $.fn.ePaginater = function (options) {
+        // TODO: extend options arg...
         var config = {
-            active: this.css('color') || '#666',
-            color: $('body').find('a').eq(0).css('color') || '#228fdd',    
             point_break: 160
         };
         Array.prototype.jam = function jam() {
             return this.join(' ');
         };
-        $.fn.chop = function () {
+        Object.prototype.chop = function chop () {
             return this.get(0).innerHTML.split(' ');
-        };
-        $.fn.css_bg = function (add) {
-            return this.css('background-color', add ? config.active : '')
-        };
-        $.fn.css_f = function (add) {
-            return this.css('color', add ? '#666' : config.color)
         };
         if (this.chop().length > config.point_break) {
             //  eBuilder
-            var eBuilder = function eBuilder(e,id,a,c,t,ch){var el=document.createElement(e);if(id){el.id=id}if(a){for(var key in a){if(a.hasOwnProperty(key)){el.setAttribute([key],a[key])}}}if(c){el.className=typeof c==='object'&&c.constructor===Array?c.join(' '):c}if(t){el.appendChild(document.createTextNode(t))}if(ch){if(!ch.length){el.appendChild(ch)}else{for(var i=0;i<ch.length;i+=1){el.appendChild(ch[i])}}}return el}
-                ,   a = 'a'
-                ,   button = 'button'
-                ,   add = 'add'
+            function eBuilder(e,id,a,c,t,ch){var el=document.createElement(e);if(id){el.id=id;}if(a){for(var key in a){if(a.hasOwnProperty(key)){el.setAttribute([key],a[key]);}}}if(c){el.className=typeof c==='object'&&c.constructor===Array?c.join(' '):c;}if(t){el.appendChild(document.createTextNode(t));}if(ch){if(!ch.length){el.appendChild(ch);}else{for(var i=0;i<ch.length;i+=1){el.appendChild(ch[i]);}}}return el;}
+
+            var button = 'button'
                 ,   count = 0
                 ,   div = 'div'
                 ,   $elem
@@ -58,132 +50,118 @@
                 ,   mouseover 
                 ,   $nav = '.nav-dots'
                 ,   on = 'on'
-                ,   on_class = '.on'
+                ,   on_class = '.'+on
                 ,   p = 'p'
                 ,   page = 'page'
                 ,   pag_prev =  'pag_prev'
                 ,   pag_next = 'pag_next'
                 ,   $wrapper = this
-                ,   insert_pag = function () {
-                        var $page = $(eBuilder(div, 'page_'+id));
-                        $.each(elems, function () {
-                            this.appendTo($page);    
-                        });
-                        $page.appendTo($wrapper);
-                        id++;
-                        elems.length = 0;    
-                    }
-                ,   paginater_loop = function ($that, i) {
-                        var this_length = $that.chop().length
-                            ,   pl_count = this_length + count
-                        ;
-                        inner = $that.chop();
-                        if (pl_count < config.point_break || pl_count === config.point_break) {
-                            elems.push($that);
-                            count += this_length;
-                        } else {
-                            var slice_point = +- ((this_length + count) - config.point_break)
-                                ,   new_p = (inner.slice(0)).slice(slice_point)
-                            ;
-                            $that.text(inner.slice(0, config.point_break - count).jam());
-                            elems.push($that);
-                            insert_pag();
-                            count = new_p.length;
-                            elems.push($(eBuilder(p, '', '', '', new_p.jam())));
-                        }
-                        if (i + 1 === master_arr.length) {
-                            insert_pag();
-                        }
-                    }
             ;
+            function insert_pag() {
+                var $page = $(eBuilder(div, 'page_'+id));
+                $.each(elems, function () {
+                    this.appendTo($page);    
+                });
+                $page.appendTo($wrapper);
+                id += 1;
+                elems.length = 0;
+            }
+            function paginater_loop($that, i) {
+                var this_length = $that.chop().length
+                    ,   pl_count = this_length + count
+                ;
+                inner = $that.chop();
+                if (pl_count < config.point_break || pl_count === config.point_break) {
+                    elems[elems.length] = $that;
+                    count += this_length;
+                } else {
+                    var slice_point = (this_length + count) - config.point_break
+                        ,   new_p = inner.slice(config.point_break - count)
+                    ;
+                    $that.text(inner.slice(0, config.point_break - count).jam());
+                    elems[elems.length] = $that;
+                    insert_pag();
+                    count = new_p.length;
+                    if (count > config.point_break) {
+                        while (new_p.length > config.point_break) {
+                            var newer_p = new_p.splice(0, config.point_break).jam();
+                            elems[elems.length] = $(eBuilder(p, 0, 0, 0, newer_p));
+                            insert_pag();                        
+                        }
+                    }
+                    elems[elems.length] = $(eBuilder(p, 0, 0, 0, new_p.jam()));
+                }
+                if (i + 1 === master_arr.length) {
+                    insert_pag();
+                }
+            }
             $wrapper.find(p).each( function (i) {
                 paginater_loop($(this), i);
             });
             if ($wrapper.find(div).size() > 1) {
-                $(eBuilder('ul', '', '', $nav.split('.')[1], '', [eBuilder(li, '', '', '', '', eBuilder(button, pag_prev, 0, '', '< previous')), eBuilder(li, '', '', '', '', eBuilder(button, pag_next, 0, '', 'next >'))])).appendTo(this);
+                $(eBuilder('ul', 0, 0, $nav.split('.')[1], 0, [eBuilder(li, 0, 0, 0, 0, eBuilder(button, pag_prev, 0, 'directional', '< previous')), eBuilder(li, 0, 0, 0, 0, eBuilder(button, pag_next, 0, 'directional', 'next >'))])).appendTo(this);
                 $nav = $($nav);
                 for (i = 0; i < this.find(div).length; i++) {
-                    lis.push($(eBuilder(li, '', '', '', '', eBuilder(button, 'dot_'+i, 0, '', i+1))).data(page, true));
+                    lis[lis.length] = $(eBuilder(li, 0, 0, 0, 0, eBuilder(button, 'dot_'+i, 0, 0, i+1))).data(page, true);
                 }
             }
             $.each(lis, function () {
                 $nav.children().last().before(this);
             });
-            $nav.find(li).css({float: 'left', 'margin-right': '4px'}); 
-            $nav.find(button).css({border: '1px solid #eee', color: config.color, padding: '4px'});
-            $(lis[0]).find(button).addClass(on).css_f(add);
+            $(lis[0]).find(button).addClass(on);
             $(document.getElementById('page_0')).addClass(on).show();
             $nav.find(button).hover(
                 function () {
-                    var $this = $(this);
-                    mouseover = true;
-                    $elem = $nav.find(on_class).parent();
-                    if (this.id === pag_prev || this.id === pag_next) {
-                        if (this.id === pag_prev && $elem.prev().data(page)){
-                            $this.css_bg(add);
-                            $elem.prev().find(button).css_bg(add);
-                        }    
-                        if (this.id === pag_next && $elem.next().data(page)){
-                            $this.css_bg(add);
-                            $elem.next().find(button).css_bg(add);
-                        }                         
-                    } else {
-                        if ($this.attr('class') !== on) {
-                            $this.css_bg(add);
-                        }
+                    if ($(this).hasClass('directional')) {
+                        mouseover = true;
+                        $elem = $nav.find(on_class).parent();
+                        if ((this.id === pag_prev && $elem.prev().data(page) === undefined) || (this.id === pag_next && $elem.next().data(page) === undefined)) {
+                            $(this).addClass('off');                    
+                        }           
                     }
                 },
                 function () {
-                    $elem = $nav.find(on_class).parent();
-                    mouseover = false; 
-                    $(this).css_bg();
-                    if (this.id === pag_prev && $elem.prev().data(page)) {
-                        $elem.prev().find(button).css_bg();
-                    }
-                    if (this.id === pag_next && $elem.next().data(page)) {
-                        $elem.next().find(button).css_bg();
+                    if ($(this).hasClass('directional')) {
+                        mouseover = false; 
+                        $elem = $nav.find(on_class).parent();
+                        if ((this.id === pag_prev && $elem.prev().data(page) === undefined) || (this.id === pag_next && $elem.next().data(page) === undefined)) {
+                            $(this).removeClass('off');                    
+                        }
                     }
                 }            
-            );
-            $nav.find(button).click( function () {
+            ).click( function () {
+                function pag_click(that) {
+                    $(document.getElementById('page_'+that.split('_')[1])).addClass(on).show();                                
+                }
+                function scroll(direction) {
+                    $nav.find(on_class).removeClass(on);
+                    $wrapper.find(on_class).removeClass(on_class).hide();
+                    $(document.getElementById(direction)).addClass(on);
+                    pag_click(direction);
+                }                    
                 var scroll_back = $nav.find(on_class).parent().prev().data(page)
                     ,   scroll_fwd = $nav.find(on_class).parent().next().data(page)
-                    ,   pag_click = function (that) {
-                            $(document.getElementById('page_'+that.split('_')[1])).addClass(on).show();                                
-                    }
-                    ,   scroll = function (direction) {
-                            $nav.find(on_class).removeClass(on).css('color', config.color);
-                            $wrapper.find(on_class).removeClass(on_class).hide();
-                            $(document.getElementById(direction)).addClass(on).css_f(add).css_bg();
-                            pag_click(direction);
-                    }
                 ;
-                if (this.id === pag_prev) {
-                    if (scroll_back) {
-                        scroll($nav.find(on_class).parent().prev().find(button).attr('id'));
-                        if (mouseover) {
-                            $nav.find(on_class).parent().prev().find(button).css_bg(add);
-                        }
-                        if ($nav.find(on_class).parent().index() === 1) {
-                            $(document.getElementById(pag_prev)).css_bg();
-                        }
+                if (this.id === pag_prev && scroll_back) {
+                    scroll($nav.find(on_class).parent().prev().find(button).attr('id'));
+                    if (mouseover) {
+
                     }
-                } else if (this.id === pag_next) {
-                    if (scroll_fwd) {
-                        scroll($nav.find(on_class).parent().next().find(button).attr('id'));
-                        if (mouseover) {
-                            $nav.find(on_class).parent().next().find(button).css_bg(add);
-                        }
-                        if ($nav.find(on_class).parent().next().is(':last-child')) {
-                            $(document.getElementById(pag_next)).css_bg();
-                        }
-                    } else {
-                        $(document.getElementById(pag_next)).css_bg();
+                    if ($nav.find(on_class).parent().index() === 1) {
+
+                    }
+                } else if (this.id === pag_next && scroll_fwd) {
+                    scroll($nav.find(on_class).parent().next().find(button).attr('id'));
+                    if (mouseover) {
+
+                    }
+                    if ($nav.find(on_class).parent().next().is(':last-child')) {
+
                     }
                 } else {
-                    $nav.find(on_class).removeClass(on).css_f();
+                    $nav.find(on_class).removeClass(on);
                     $wrapper.find(on_class).hide();
-                    $(this).addClass(on).css_f(add).css_bg();
+                    $(this).addClass(on);
                     pag_click(this.id);
                 }
             });
@@ -198,4 +176,3 @@
         return this;
 	};
 })(jQuery);
-
